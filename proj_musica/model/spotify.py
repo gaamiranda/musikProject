db3a21383c047f8da4f42d7890ce50fe58e29967
gaamiranda <145232395+gaamiranda@users.noreply.github.com>
2024-08-01 23:18:50 +0100

    Update spotify.py

--- proj_musica/model/spotify.py
+++ proj_musica/model/spotify.py
@@ -4,8 +4,8 @@ from tkinter import messagebox
 
 def spoti_play(uri):
 
-	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="a2656aab5a0f45d1b73d45d80f8cf09d",
-												client_secret="a5ad97e3877b454cae5bc1f75ce1b450",
+	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="",
+												client_secret="",
 												redirect_uri="http://localhost:1234",
 												scope="user-library-read user-modify-playback-state user-read-playback-state"))
 
@@ -19,8 +19,8 @@ def spoti_play(uri):
 	sp.start_playback(device_id=device_id, uris=[uri])
 
 def spoti_pause():
-	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="a2656aab5a0f45d1b73d45d80f8cf09d",
-												client_secret="a5ad97e3877b454cae5bc1f75ce1b450",
+	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="",
+												client_secret="",
 												redirect_uri="http://localhost:1234",
 												scope="user-library-read user-modify-playback-state user-read-playback-state"))
 
@@ -34,8 +34,8 @@ def spoti_pause():
 	sp.pause_playback(device_id=device_id)
 
 def spoti_resume():
-	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="a2656aab5a0f45d1b73d45d80f8cf09d",
-												client_secret="a5ad97e3877b454cae5bc1f75ce1b450",
+	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="",
+												client_secret="",
 												redirect_uri="http://localhost:1234",
 												scope="user-library-read user-modify-playback-state user-read-playback-state"))
 
